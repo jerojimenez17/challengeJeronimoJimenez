85c30384ebc2a67edcd409a22e864a07f46ff031
jerojimenez17 <jerodepiran@gmail.com>
2023-10-04 17:44:30 -0300

    creating select component

---
+++ challenge/src/components/Select.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+const Select = (props) => {
+  return (
+    <div className="row mt-4 mb-5">
+      <select
+        className="form-select bg-dark text-white "
+        onChange={(e) => props.handleFilterChange(e.target.value)}
+      >
+        <option value="">Seleccione categoria...</option>
+        {props.categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
