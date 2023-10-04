57d1d0213c9d3aa6c66a37252531483823275db7
jerojimenez17 <jerodepiran@gmail.com>
2023-10-04 18:10:56 -0300

    change select style

--- challenge/src/components/Select.jsx
+++ challenge/src/components/Select.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Select = (props) => {
   return (
-    <div className="row mt-4 mb-5">
+    <div className="row mt-4 mb-5 shadow-lg rounded">
       <select
         className="form-select bg-dark text-white "
         onChange={(e) => props.handleFilterChange(e.target.value)}
