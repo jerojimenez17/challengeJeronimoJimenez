cd6b461635f2cf6fa2435b22a3e8f2def3b5f412
jerojimenez17 <jerodepiran@gmail.com>
2023-10-05 09:34:50 -0300

    adding enviroment variable

--- challenge/src/services/ProductsService.js
+++ challenge/src/services/ProductsService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const fetchProducts = async () => {
   try {
     // eslint-disable-next-line no-undef
-    const response = await axios.get("https://fakestoreapi.com/products", {
+    const response = await axios.get(import.meta.env.VITE_API_URL, {
       headers: {
         "Content-Type": "application/json",
       },
